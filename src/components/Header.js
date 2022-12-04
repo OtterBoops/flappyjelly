ee9bd1c3814d97fc7c7e4ba322a9dfca55a20e40
OtterBoops <me44888@gmail.com>
2022-12-04 17:39:56 +0200

    Add firefox warning. Can't be bothered.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,15 @@
+var isFirefox = typeof InstallTrigger !== 'undefined';
+
 export default function Header() {
   return (
-    <div className='flex justify-center items-center bg-jellyPink p-4 rounded-lg my-5 w-11/12 Shadow text-xl s:text-center'>
+    <div className='flex justify-center flex-col items-center bg-jellyPink p-4 rounded-lg my-5 w-11/12 Shadow text-xl s:text-center'>
+      <p className='font-bold'>Flappy Jelly</p>
       <p>When she quacks, she jumps and when she jumps she quacks.</p>
+      {isFirefox && (
+        <p className='text-red-500 text-lg font-bold'>
+          Flappy Jelly runs like shit on Firefox. Use Chrome/Opera &lt;3
+        </p>
+      )}
     </div>
   );
 }
