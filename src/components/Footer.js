870a3b56e047893fe8071d0c4f5f22a75104189d
OtterBoops <me44888@gmail.com>
2022-12-08 13:21:50 +0200

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,9 @@ export default function Footer() {
     <div
       className='flex justify-center items-center bg-jellyBlue my-4 p-4 rounded-lg
      w-11/12 Shadow'>
-      <p>I am a footer</p>
+      <p>
+        Happy holidays, bitch. Not sorry for the little troll. Enjoy the game!
+      </p>
     </div>
   );
 }
